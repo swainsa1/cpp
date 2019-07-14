1c91f99b6ed345dcda1b96bb80307c25df63ff09
Santosh Swain <swainsa1@Santoshs-MacBook-Pro-2.local>
2019-07-14 01:35:41 -0500

    Added map example

--- STLLibs/Main.cpp
+++ STLLibs/Main.cpp
@@ -10,12 +10,42 @@
 #include <fstream>
 #include <vector>
 #include <list>
+#include <map>
 #include <stdlib.h>
 
 using namespace std;
 
+class Person
+{
+private:
+	string name;
+	int age;
+
+public:
+	Person() : name(""), age(0)
+	{
+	}
+	Person(const Person &other)
+	{
+		name = other.name;
+		age = other.age;
+	}
+
+	Person(string name, int age) : name(name), age(age)
+	{
+	}
+
+	void print (){
+		cout << " { \n \t\t\t Name : "+name +" , Age : " << age << " \n\t\t\t}"; 
+	}
+
+};
+
+void printMap(map<int, Person> map);
 void matrixVector();
 void listExample();
+void mapExample();
+void printMap(map<int, Person> map);
 void printList(list<int> list1);
 void addToList(list<int> list1, int value);
 list<int> removeFromeList(list<int> list1, int value);
@@ -24,8 +54,23 @@ int main()
 {
 	matrixVector();
 	listExample();
+	mapExample();
 
 	return 0;
+}
+
+void mapExample()
+{
+
+	map<int, Person> person_map;
+	person_map[1001] = Person("Alex Misso", 40);
+	person_map[1002] = Person("Joane Buter", 38);
+	person_map[1003] = Person("Minnea polis", 43);
+	person_map[1004] = Person("Jaha bulder", 85);
+
+	person_map.insert(make_pair(1005, Person("Utsav Fasino", 55)));
+
+	printMap(person_map);
 }
 
 void listExample()
@@ -55,17 +100,10 @@ void listExample()
 
 		if (*it_modify == 1031 || *it_modify == 1051 || *it_modify == 1081)
 		{
-			list1.push_back(*it_modify+500);
+			list1.push_back(*it_modify + 500);
 			list1.erase(it_modify);
-			
 		}
-
-			
 	}
-
-
-
-
 
 	printList(list1);
 }
@@ -79,6 +117,20 @@ void printList(list<int> list1)
 		cout << *it << " ";
 	}
 	cout << " } " << endl;
+}
+
+void printMap(map<int, Person> person_map)
+{
+	cout << " Map Size[" << person_map.size() << "] { ";
+	for (map<int, Person>::iterator it = person_map.begin(); it != person_map.end(); it++)
+	{
+
+		cout << "\n\t\t" << it->first << " : " << flush ;
+		it->second.print();
+		
+
+	}
+	cout << " \n\t} " << endl;
 }
 
 void matrixVector()
