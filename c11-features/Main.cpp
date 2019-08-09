c64ff424f8ee72b28bd2450139c77d972a6aa3e0
Santosh Swain <swainsa1@Santoshs-MacBook-Pro-2.local>
2019-08-08 22:02:12 -0500

    adding recent changes

--- c11-features/Main.cpp
+++ c11-features/Main.cpp
@@ -29,6 +29,7 @@ void lambdaFunctionWithParam();
 void lambdaFunctionWithPointerFunction();
 
 void lambdaFunctionWithVariables();
+void uniquePointer();
 
 class Person
 {
@@ -39,9 +40,11 @@ private:
 public:
 	Person() : name(""), age(0)
 	{
+		cout << " constructor  Person() " << endl;
 	}
 	Person(const Person &other)
 	{
+		cout << " Person(const Person &other) " << endl;
 		name = other.name;
 		age = other.age;
 	}
@@ -50,7 +53,23 @@ public:
 		return name < other.name;
 	}
 
-	Person(string name, int age) : name(name), age(age) {}
+	Person operator=(const Person &other) const
+	{
+		return *this;
+	}
+
+	Person operator=(const Person *other) const
+	{
+		cout << "------  " << endl;
+		other->printSimple();
+		cout << "------  " << endl;
+		return *other;
+	}
+
+	Person(string name, int age) : name(name), age(age)
+	{
+		cout << " Person(string name, int age) " << endl;
+	}
 
 	void print() const
 	{
@@ -60,6 +79,11 @@ public:
 	void printSimple() const
 	{
 		cout << "  Name : " + name + " , Age : " << age << " \n";
+	}
+
+	~Person()
+	{
+		cout << "Destructor" << endl;
 	}
 
 	friend bool compByAge(const Person aPerson, Person anotherPerson);
@@ -87,6 +111,8 @@ int main()
 	lambdaFunctionWithPointerFunction();
 
 	lambdaFunctionWithVariables();
+
+	uniquePointer();
 
 	return 0;
 }
@@ -163,6 +189,128 @@ void proxyFuntion(int (*aFunctionPointer)(int, int))
 	cout << aFunctionPointer(10, 20) << endl;
 }
 
+void uniquePointer()
+{
+
+	cout << " Regular Start scope " << endl;
+	{
+
+		Person *aPerson_pointer = new Person();
+		aPerson_pointer->printSimple();
+	}
+	cout << " End scope " << endl;
+
+	cout << " Unique Start scope " << endl;
+	{
+
+		unique_ptr<Person> aPtr_unique_person(new Person);
+
+		(*aPtr_unique_person).printSimple();
+	}
+	cout << " End scope " << endl;
+
+	cout << " Unique Array Start scope " << endl;
+	{
+
+		unique_ptr<Person[]> aPtr_unique_person(new Person[2]);
+
+		//for(int i=0 ;i< 2;i++){
+
+		Person *p1 = new Person("sss", 20);
+
+		aPtr_unique_person[0] = p1;
+
+		aPtr_unique_person[0].printSimple();
+
+		//}
+
+		const char *charArrayPtr = {" Hello how are you "};
+		cout << charArrayPtr << endl;
+		charArrayPtr = "X";
+		cout << charArrayPtr << endl;
+
+		size_t value;
+
+		//cin >> value;
+
+		cout << "\n You have entered " << value << endl;
+
+		//Pointer initializaionß
+
+
+		// //double* ptr1{};
+
+		// cout << "Ptr with *ptr1{} " << *ptr1 << endl;
+
+		// delete ptr1;
+
+		double *ptr2{new double{}};
+
+		cout << "*ptr2{new double{}} " << *ptr2 << endl;
+
+		delete ptr2;
+
+
+
+
+		//Array 
+
+		double* data {new double[100]{1.2}};
+
+		for(size_t i=0;i<100;i++){
+
+			cout << "i= "<<data[i]<<endl;
+
+		}
+
+		delete [] data;
+		data = nullptr;
+
+
+		// //Two dimensional array pointer 
+
+		// double** carrots{ new double*[2] {} };
+
+		// carrots[1][1]= 5;
+
+		// cout << "[0][0] " << carrots[0][0] <<endl;
+		// cout << "[0][1] " << carrots[0][1] <<endl;
+		// cout << "[1][0] " << carrots[1][0] <<endl;
+		// cout << "[1][1] " << carrots[1][1] <<endl;
+
+
+		vector<Person>* pData {new std::vector<Person>};
+
+		 
+
+		pData -> push_back(Person("Brady Swain",12));
+
+		(*pData).push_back(Person("Tom Swain",21));
+
+
+
+
+
+
+
+	
+
+
+	}
+
+	
+	cout << " End scope " << endl;
+
+	// cout << " Auto Start scope " << endl;
+	// {
+
+	// 	auto<Person> aPtr_unique_person(new Person);
+
+	// 	(*aPtr_unique_person).printSimple();
+	// }
+	// cout << " End scope " << endl;
+}
+
 void lambdaFunctionWithPointerFunction()
 {
 
@@ -180,7 +328,7 @@ void lambdaFunctionWithPointerFunction()
 
 void lambdaFunctionWithVariables()
 {
-	//pass value 
+	//pass value
 	{
 		int var1 = 100;
 		int var2 = 200;
@@ -201,49 +349,44 @@ void lambdaFunctionWithVariables()
 		int var3 = 300;
 		//All / by value  /
 		auto lfunc = [&]() {
-			var1 =var1 +1000;
-			var2 =var2 +1000;
-			var3 =var3 +1000;
+			var1 = var1 + 1000;
+			var2 = var2 + 1000;
+			var3 = var3 + 1000;
 			cout << "var1 = " << var1 << endl;
 			cout << "var2 = " << var2 << endl;
 			cout << "var3 = " << var3 << endl;
 		};
 		lfunc();
-		
 	}
 
-		//Mix 1
+	//Mix 1
 	{
 		int var1 = 100;
 		int var2 = 200;
 		int var3 = 300;
 		//All / by value  /
-		auto lfunc = [& ,var1]() {
-			 
-			var2 =var2 +1000;
-			var3 =var3 +1000;
+		auto lfunc = [&, var1]() {
+			var2 = var2 + 1000;
+			var3 = var3 + 1000;
 			cout << "var1 = " << var1 << endl;
 			cout << "var2 = " << var2 << endl;
 			cout << "var3 = " << var3 << endl;
 		};
 		lfunc();
-		
 	}
 
-			//Mix 2
+	//Mix 2
 	{
 		int var1 = 100;
 		int var2 = 200;
 		int var3 = 300;
 		//All / by value  /
-		auto lfunc = [= ,&var1]() {
-			 
-			var1 =5000;
+		auto lfunc = [=, &var1]() {
+			var1 = 5000;
 			cout << "var1 = " << var1 << endl;
 			cout << "var2 = " << var2 << endl;
 			cout << "var3 = " << var3 << endl;
 		};
 		lfunc();
-		
 	}
 }
