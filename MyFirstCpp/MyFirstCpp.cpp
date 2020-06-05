d0d0e7466b6d446bc9027356174b514d5e2de214
Santosh Swain <swainsa1@Santoshs-MacBook-Pro-2.local>
2020-06-05 14:09:44 -0500

    Checking in my lib

--- MyFirstCpp/MyFirstCpp.cpp
+++ MyFirstCpp/MyFirstCpp.cpp
@@ -6,10 +6,12 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 #include<iostream>
+#include<boost/locale.hpp>
 
 using namespace std;
 void passByValue(int value);
 void passByReference(int* value);
+void localeTest();
 int main() {
 	cout << "Starting " << endl; 
 
@@ -22,10 +24,18 @@ int main() {
      cout << "Main 2 . " << var << endl;;
      passByValue(var);
 	 cout << "Main  . " << var << endl;;
+
+	 localeTest();
 	return 0;
 }
 
+class A;
 
+
+class A {
+   int id;
+   std::variant<int,unique_ptr<A()>> var;
+};
 void passByValue(int value){
  cout << "passByValue . 1 .Received value = " << value << endl;;
  value = 100 ;
@@ -36,5 +46,27 @@ void passByReference(int* value){
  cout << "passByReference 1. Received value = " << *value << endl;;
  *value = 500 ;
  cout << "passByReference 2 .Reset value = " << *value << endl;; 
+
+}
+
+
+void localeTest(){
+
+  std::locale defaultLocale;     // the "C" locale
+  defaultLocale.global(std::locale(""));
+  cout << "-------" << defaultLocale.name()<<endl;
+
+
+  		setlocale(LC_ALL, "");
+    cout << "LC_ALL: " << setlocale(LC_ALL, NULL) << endl;
+    cout << "LC_CTYPE: " << setlocale(LC_CTYPE, NULL) << endl;
+    
+ using namespace boost::locale;
+    using namespace std;
+    generator gen;
+    // Make system default locale global
+    std::locale loc = gen("");
+	cout << "-------" << loc.name()<<endl;
+
 
 }
